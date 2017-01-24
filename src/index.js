d169682f6754edbc953d4560c28004547a66ef08
noprysk <noprysk@n-ix.com>
2017-01-24 18:27:04 +0200

    Redux implement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import { createStore } from 'redux';
+import todoApp from './reducers/ToDoReducer';
+
+import App from './components/App';
 import './index.css';
+
+
+let store = createStore(todoApp);
 
 ReactDOM.render(
   <App />,
